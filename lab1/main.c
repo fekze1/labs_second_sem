a4c6de9d6f8264b8de6331ebd5c1d9f201560363
fekze1 <roma.pimkin.777@gmail.com>
2023-03-19 02:58:41 +0300

    test

--- lab1/main.c
+++ lab1/main.c
@@ -9,12 +9,12 @@
 int main()
 {
     complex_array test1 = create_cmplx_arr(10);
-    float_array test2 = create_flt_arr(10);
+    float_array test22 = create_flt_arr(10);
 
     add_complex(&test1, (complex){1, 1});
-    add_float(&test2, 2.12412);
+    add_float(&test22, 2.12412);
 
     delete_data(test1.data);
-    delete_data(test2.data);
+    delete_data(test22.data);
     return 0;
 }
